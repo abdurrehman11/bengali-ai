16935a361132fbc299804d416fe63deb477745da
abdurrehman <abdurrehman245@gmail.com>
2020-03-01 00:53:13 +0500

    train.py in process

--- src/train.py
+++ src/train.py
@@ -1,6 +1,11 @@
-import os
-import ast
 # https://stackoverflow.com/questions/15197673/using-pythons-eval-vs-ast-literal-eval
+
+import os
+import ast  # converts a Stringified list to a normal list
+import torch
+from torch import nn
+from model_dispatcher import MODEL_DISPATCHER
+from dataset import BengaliDatasetTrain
 
 DEVICE = "cuda"
 
@@ -20,3 +25,58 @@ TRAINING_FOLDS = ast.literal_eval(os.environ.get("TRAINING_FOLDS"))
 VALIDATION_FOLDS = ast.literal_eval(os.environ.get("VALIDATION_FOLDS"))
 
 BASE_MODEL = int(os.environ.get("BASE_MODEL"))
+
+def main():
+    model = MODEL_DISPATCHER[BASE_MODEL](pretrained=True)
+    model.to(DEVICE)
+    
+    train_dataset = BengaliDatasetTrain(
+        folds=TRAINING_FOLDS,
+        img_height=IMG_HEIGHT,
+        img_width=IMG_WIDTH,
+        mean=MODEL_MEAN,
+        std=MODEL_STD
+    )
+    
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset,
+        batch_size=TRAIN_BATCH_SIZE,
+        shuffle=True,
+        num_workers=4
+    )
+    
+    valid_dataset = BengaliDatasetTrain(
+        folds=VALIDATION_FOLDS,
+        img_height=IMG_HEIGHT,
+        img_width=IMG_WIDTH,
+        mean=MODEL_MEAN,
+        std=MODEL_STD
+    )
+    
+    valid_loader = torch.utils.data.DataLoader(
+        dataset=valid_dataset,
+        batch_size=TEST_BATCH_SIZE,
+        shuffle=False,
+        num_workers=4
+    )
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    
+    # https://pytorch.org/docs/stable/_modules/torch/optim/lr_scheduler.html
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max',
+                                                           patience=5, factor=0.3, verbose=True)
+    
+    # to parallelize the model on multiple GPU's
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel()
+        
+    
+    for epoch in range(EPOCHS):
+        train()
+        val_score = evaluate()
+        scheduler.step(val_score)
+        torch.save(model.state_dict(), f"{BASE_MODEL}_fold{VALIDATION_FOLDS[0]}.bin")
+    
+    
+if __init__ == "__main__":
+    main()
